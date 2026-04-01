9809e14440276a03d824208ef045382116e0661a
LEGION <LEGION@DESKTOP-VKAQ8VN>
2026-04-01 10:59:41 +0530

    rename to branch1 in github.com

--- SampleHello6/src/main/java/com/durgasoft/SampleHello6Application.java
+++ SampleHello6/src/main/java/com/durgasoft/SampleHello6Application.java
@@ -10,6 +10,7 @@ public class SampleHello6Application {
 		SpringApplication.run(SampleHello6Application.class, args);
 		System.out.println("This is SampleHello6");
 		System.out.println("This is branch1 under main branch");
+		System.out.println("Finally I renamed to branch1 in github.com");
 	}
 
 }
