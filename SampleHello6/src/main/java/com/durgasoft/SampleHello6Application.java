06c5f2d98927a426c4f2412867593908c1bfe953
LEGION <LEGION@DESKTOP-VKAQ8VN>
2026-04-01 09:46:11 +0530

    first Commit in branch3

--- SampleHello6/src/main/java/com/durgasoft/SampleHello6Application.java
+++ SampleHello6/src/main/java/com/durgasoft/SampleHello6Application.java
@@ -9,6 +9,7 @@ public class SampleHello6Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SampleHello6Application.class, args);
 		System.out.println("This is SampleHello6");
+		System.out.println("This is branch2");
 	}
 
 }
