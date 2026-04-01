7ae228054a3ee3834ce76c527df3ed4e98d8f9b3
LEGION <LEGION@DESKTOP-VKAQ8VN>
2026-04-01 09:32:01 +0530

    First commit in SampleHello6

---
+++ SampleHello6/src/main/java/com/durgasoft/SampleHello6Application.java
@@ -0,0 +1,14 @@
+package com.durgasoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleHello6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleHello6Application.class, args);
+		System.out.println("This is SampleHello6");
+	}
+
+}
