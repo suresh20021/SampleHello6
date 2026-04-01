1d4fa79e35a4fa0c617ef7a52b5608d0164996ed
LEGION <LEGION@DESKTOP-VKAQ8VN>
2026-04-01 11:12:17 +0530

    added in Branch1  Branch1

--- SampleHello6/src/main/java/com/durgasoft/SampleHello6Application.java
+++ SampleHello6/src/main/java/com/durgasoft/SampleHello6Application.java
@@ -11,6 +11,7 @@ public class SampleHello6Application {
 		System.out.println("This is SampleHello6");
 		System.out.println("This is branch1 under main branch");
 		System.out.println("Finally I renamed to branch1 in github.com");
+		System.out.println("Added in Branch1 Branch1 ");
 	}
 
 }
